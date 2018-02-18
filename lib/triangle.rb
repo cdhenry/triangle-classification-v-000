d46b589c9517db87769ebedd45cc1475f022098f
Chris Henry <cdhenry@gmail.com>
2018-02-18 14:47:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
         puts error.message
       end
     else
-      if @side1 == @side2 == @side3
+      if @side1 == @side2 && @side2 == @side3
         return :equilateral
       elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
         return :isosceles
